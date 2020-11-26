95b1e0ee4d8be930e6935b641782d99a5ac618e3
devzons <devzons@gmail.com>
2020-11-25 19:43:05 -0800

    POST, GET category, owner

---
+++ server/routes/owner.js
@@ -0,0 +1,44 @@
+const router = require('express').Router()
+const Owner = require('../models/Owner')
+
+const upload = require('../middlewares/upload-photo')
+
+// POST request
+router.post('/owners', upload.single('photo'), async (req, res) => {
+  try {
+    const owner = new Owner()
+    owner.name = req.body.name
+    owner.about = req.body.about
+    owner.photo = req.file.location
+
+    await owner.save()
+
+    res.json({
+      success: true,
+      message: 'A new owner created successfully',
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+})
+
+// GET request
+router.get('/owners', async (req, res) => {
+  try {
+    let owners = await Owner.find()
+    res.json({
+      success: true,
+      owners: owners,
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    })
+  }
+})
+
+module.exports = router
