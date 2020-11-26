06eb6aa27c7331f6b50d4fd2389839c875d3ce7a
devzons <devzons@gmail.com>
2020-11-26 05:00:05 -0800

    install nuxtjs

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -2,7 +2,9 @@
   <div class="container">
     <div>
       <Logo />
-      <h1 class="title">admin</h1>
+      <h1 class="title">
+        admin
+      </h1>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
@@ -21,13 +23,51 @@
           GitHub
         </a>
       </div>
-      <a href="#" class="a-button-buy-again">Buy Now</a>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
-<style></style>
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
