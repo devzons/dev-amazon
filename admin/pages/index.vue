2fe01e066a407d872e9131b8e6d049a71d9f56fb
devzons <devzons@gmail.com>
2020-11-26 04:46:25 -0800

    install nuxtjs, setup static css, font, img

---
+++ admin/pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">admin</h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+      <a href="#" class="a-button-buy-again">Buy Now</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
