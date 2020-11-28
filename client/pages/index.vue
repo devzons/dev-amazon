db5e96942d99bd77e7bcdd57d6ece4971038cdb1
devzons <devzons@gmail.com>
2020-11-28 06:58:31 -0800

    setup client and styles

---
+++ client/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <nust-link to="/products" class="a-button-buy-again"
+      >Add a new product</nust-link
+    >
+    <div>
+      <Logo />
+      <h1 class="title">
+        client
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
